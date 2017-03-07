f6099b9ce1ee4f11f072a37cffa71daa1f936f49
Steve Westwood <steve.westwood@gmail.com>
2017-03-07 12:35:50 +0000

    simple use of signal within asp.net project

---
+++ Backend/SignalR.Api/SignalR.Api/Hubs/ScoringHub.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using SignalR.Api.Models;
+
+namespace SignalR.Api.Hubs
+{
+    [HubName("Scoring")]
+    public class ScoringHub : Hub
+    {
+        public void GetScores(int numberOfScores)
+        {
+            var scores = Score.GetScoreData(numberOfScores);
+            for (var s = 0; s < numberOfScores; s++)
+            {
+                Thread.Sleep(2000);
+                Clients.All.onGetScores(scores[s]);
+            }
+        }
+    }
+}
---
+++ Backend/SignalR.Api/SignalR.Api/Models/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace SignalR.Api.Models
+{
+    public class Score
+    {
+        public int Id { get; set; }
+        public string ScoreText { get; set; }
+
+        public Score(int id, string scoreText)
+        {
+            Id = id;
+            ScoreText = scoreText;
+        }
+
+        public static Score[] GetScoreData(int numberOfScores)
+        {
+            var scores = new List<Score>();
+            for (var i = 1; i <= numberOfScores; i++)
+            {
+                scores.Add(new Score(i, $"Score for loan {i}"));
+            }
+            return scores.ToArray();
+        }
+    }
+}
